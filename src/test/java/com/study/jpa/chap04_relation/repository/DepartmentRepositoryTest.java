e0f607bc140be227bd6b38d22d541664cee36b8c
yongseopK <kk0@kakao.com>
2024-01-17 10:14:00 +0900

    #8. JPA 양뱡향 연관관계 - 수정 시 주의사항

--- src/test/java/com/study/jpa/chap04_relation/repository/DepartmentRepositoryTest.java
+++ src/test/java/com/study/jpa/chap04_relation/repository/DepartmentRepositoryTest.java
@@ -1,6 +1,7 @@
 package com.study.jpa.chap04_relation.repository;
 
 import com.study.jpa.chap04_relation.entity.Department;
+import com.study.jpa.chap04_relation.entity.Employee;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,5 +42,32 @@ class DepartmentRepositoryTest {
 
         System.out.println("\n\n\n\n");
     }
+
+    @Test
+    @DisplayName("양방향 연관관계에서 연관데이터 수정")
+    void changeDeptTest() {
+        //given
+        // 3번 사원의 부서를 2번인에서 1번으로 수정해야함
+
+        Employee employee = employeeRepository.findById(3L).orElseThrow();
+
+        Department department = departmentRepository.findById(1L).orElseThrow();
+        //when
+
+        // tbl_emp테이블에 dept_id를 변경는것이 논리적으로 맞다.
+        // ⭐ 양방향에서는 반대편도 같이 수정해야함.️
+        employee.setDepartment(department);
+        department.getEmployees().add(employee);
+        employeeRepository.save(employee);
+
+        //then
+
+        // 부서를 변경한 상태에서 Department쪽에 1번 부서 사원을 조회한다면?
+        List<Employee> employees = department.getEmployees();
+        System.out.println("\n\n\n\n");
+        System.out.println("employees = " + employees);
+        System.out.println("\n\n\n\n");
+    }
+
 
 }
